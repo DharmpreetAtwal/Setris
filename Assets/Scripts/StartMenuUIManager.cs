5c4213e95701d5621b83c334cd2ed24d1521907d
DharmpreetAtwal <88208715+DharmpreetAtwal@users.noreply.github.com>
2023-07-11 11:45:32 -0400

    Initialized Menu Code
    Start btn works. Inputted name is saved when other scene loads.

--- Assets/Scripts/StartMenuUIManager.cs
+++ Assets/Scripts/StartMenuUIManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class StartMenuUIManager : MonoBehaviour
 {
+    public static StartMenuUIManager Instance;
+    public TMP_InputField inputField;
+    public string playerName;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnStartClicked()
     {
-        
+        playerName = inputField.text;
+        SceneManager.LoadScene(0);
     }
 }
