501a6fa2a59b97a452129d644730c10e7c4cfc43
DharmpreetAtwal <88208715+DharmpreetAtwal@users.noreply.github.com>
2023-12-24 10:14:28 -0500

    ----------------

--- Assets/Scripts/CameraResolution.cs
+++ Assets/Scripts/CameraResolution.cs
@@ -1,86 +1,67 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-/// <summary>
-/// Skrypt odpowiada za usatwienie rozdzielczosci kemerze
-/// </summary>
-public class CameraResolution : MonoBehaviour
-{
-    #region Pola
-    private int ScreenSizeX = 0;
-    private int ScreenSizeY = 0;
-    #endregion
+public class CameraResolution : MonoBehaviour {
+    private int ScreenX = 0;
+    private int ScreenY = 0;
 
-    #region metody
 
-    #region rescale camera
-    private void RescaleCamera()
-    {
+    private void Resize() {
 
-        if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
+        if (Screen.width == ScreenX && Screen.height == ScreenY) {
+            return;
+        }
 
-        float targetaspect = 21.0f / 18.0f;
-        float windowaspect = (float)Screen.width / (float)Screen.height;
-        float scaleheight = windowaspect / targetaspect;
+        float tarasp = 21.0f / 18.0f;
+        float winasp = (float)Screen.width / (float)Screen.height;
+        float scaleh = winasp / tarasp;
         Camera camera = GetComponent<Camera>();
 
-        if (scaleheight < 1.0f)
-        {
+        if (scaleh < 1.0f) {
             Rect rect = camera.rect;
 
             rect.width = 1.0f;
-            rect.height = scaleheight;
+            rect.height = scaleh;
             rect.x = 0;
-            rect.y = (1.0f - scaleheight) / 2.0f;
+            rect.y = (1.0f - scaleh) / 2.0f;
 
             camera.rect = rect;
         }
-        else // add pillarbox
-        {
-            float scalewidth = 1.0f / scaleheight;
+        else {
+            float scalew = 1.0f / scaleh;
 
             Rect rect = camera.rect;
 
-            rect.width = scalewidth;
+            rect.width = scalew;
             rect.height = 1.0f;
-            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.x = (1.0f - scalew) / 2.0f;
             rect.y = 0;
 
             camera.rect = rect;
         }
 
-        ScreenSizeX = Screen.width;
-        ScreenSizeY = Screen.height;
+        ScreenX = Screen.width;
+        ScreenY = Screen.height;
     }
-    #endregion
 
-    #endregion
 
-    #region metody unity
-
-    void OnPreCull()
-    {
+    void OnPreCull() {
         if (Application.isEditor) return;
-        Rect wp = Camera.main.rect;
-        Rect nr = new Rect(0, 0, 1, 1);
+        Rect w = Camera.main.rect;
+        Rect n = new Rect(0, 0, 1, 1);
 
-        Camera.main.rect = nr;
+        Camera.main.rect = n;
         GL.Clear(true, true, Color.black);
 
-        Camera.main.rect = wp;
+        Camera.main.rect = w;
 
     }
 
-    // Use this for initialization
-    void Start()
-    {
-        RescaleCamera();
+    void Start() {
+        Resize();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        RescaleCamera();
+    void Update() {
+        Resize();
     }
-    #endregion
 }
